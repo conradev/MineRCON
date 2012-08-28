13aab07b5c9483b3f3e263889f2940efcde8bec9
Conrad Kramer <conrad@kramerapps.com>
2012-08-28 00:00:50 -0400

    Added click sound to button

--- Classes/MCButton.m
+++ Classes/MCButton.m
@@ -6,7 +6,28 @@
 //  Copyright (c) 2012 Kramer Software Productions, LLC. All rights reserved.
 //
 
+#import <AudioToolbox/AudioToolbox.h>
+
 #import "MCButton.h"
+
+SystemSoundID click;
+
+__attribute__((constructor))
+static void initialize_click_sound() {
+    NSURL *clickURL = [[NSBundle mainBundle] URLForResource:@"Click" withExtension:@"aiff"];
+    AudioServicesCreateSystemSoundID((__bridge CFURLRef)clickURL, &click);
+}
+
+__attribute__((destructor))
+static void destroy_click_sound() {
+    AudioServicesDisposeSystemSoundID(click);
+}
+
+@interface MCButton () {
+    __weak UIEvent *_lastEvent;
+}
+
+@end
 
 @implementation MCButton
 
@@ -25,6 +46,19 @@
         [self setTitleColor:[UIColor colorWithHue:0.0f saturation:0.0f brightness:0.62745f alpha:1.0f] forState:UIControlStateDisabled];        
     }
     return self;
+}
+
+- (void)sendAction:(SEL)action to:(id)target forEvent:(UIEvent *)event {
+    // Don't play more than one sound per event
+    if (![event isEqual:_lastEvent]) {
+        NSArray *actions = [self actionsForTarget:target forControlEvent:UIControlEventTouchUpInside];
+        if ([actions containsObject:NSStringFromSelector(action)]) {            
+            AudioServicesPlaySystemSound(click);
+            _lastEvent = event;
+        }
+    }
+    
+    [super sendAction:action to:target forEvent:event];
 }
 
 - (CGSize)intrinsicContentSize {
