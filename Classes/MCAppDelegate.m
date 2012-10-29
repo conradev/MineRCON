04395b15e3799f166ef4b8f922306ac27b205e2b
Conrad Kramer <conrad@kramerapps.com>
2012-10-29 01:41:21 -0400

    Cleaned up app delegate
    View heirarchy initialization is now properly done in
    application:willFinishLaunchingWithOptions: - a method new to iOS6, and
    one that I was unaware of!

--- Classes/MCAppDelegate.m
+++ Classes/MCAppDelegate.m
@@ -40,19 +40,79 @@ int ddLogLevel = LOG_LEVEL_WARN;
 
 #pragma mark - Application state changes
 
+- (BOOL)application:(UIApplication *)application willFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+    
+    // Instantiate file logger
+    _fileLogger = [[DDFileLogger alloc] init];
+    _fileLogger.maximumFileSize = (1024 * 1024); // 1 MB
+    _fileLogger.rollingFrequency = 0;
+    _fileLogger.logFileManager.maximumNumberOfLogFiles = 5;
+    
+    // Add default log drains
+    [DDLog addLogger:_fileLogger];
+    [DDLog addLogger:[DDASLLogger sharedInstance]];
+#if defined(CONFIGURATION_Debug) || defined(CONFIGURATION_AdHoc)
+    [DDLog addLogger:[DDTTYLogger sharedInstance]];
+#endif
+    
+    DDLogInfo(@"(%@): Instantiating file logger: %@", [[UIApplication sharedApplication] delegate], _fileLogger);
+    
+    // Roll log file (once upon every application start)
+    [_fileLogger performSelector:@selector(currentLogFileHandle)];
+    [_fileLogger rollLogFile];
+    [_fileLogger performSelector:@selector(currentLogFileHandle)];
+    
+    DDLogInfo(@"(%@): Rolled log file for logger: %@", [[UIApplication sharedApplication] delegate], _fileLogger);
+    DDLogInfo(@"(%@): Instantiating view heirarchy", [[UIApplication sharedApplication] delegate]);
+    
+    _window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
+    
+    // Create the master list view controller if it doesn't exist
+    _listViewController = [[MCServerListViewController alloc] init];
+    _listViewController.restorationIdentifier = MCServerListIdentifier;
+    
+    _navigationController = [[UINavigationController alloc] initWithRootViewController:_listViewController];
+    _navigationController.restorationIdentifier = MCMasterNavigationIdentifier;
+    
+    if ([[UIDevice currentDevice] userInterfaceIdiom] == UIUserInterfaceIdiomPhone) {
+        // Make the master view controller the root view controller
+        _listViewController.detailNavigationController = _navigationController;
+        _window.rootViewController = _navigationController;
+    } else {
+        // Create the detail view controller
+        UINavigationController *detailNavigationController = [[UINavigationController alloc] init];
+        _listViewController.detailNavigationController = detailNavigationController;
+    	
+        // Create a split view controller with master and detail view controllers
+        _splitViewController = [[UISplitViewController alloc] init];
+        _splitViewController.restorationIdentifier = MCSplitViewIdentifier;
+        _splitViewController.delegate = _listViewController;
+        _splitViewController.viewControllers = @[_navigationController, detailNavigationController];
+        
+        // Make the split view controller the root view controller
+        _window.rootViewController = _splitViewController;
+    }
+    
+    // Global appearance tweaks
+    [[UINavigationBar appearance] setTintColor:[UIColor darkGrayColor]];
+    
+    return YES;
+}
+
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-    // Set up logging
-    [self setupLogging];
-    
-    // Set up view controllers
-    [self setupViewControllers];
-    
+
     // Set up HockeyApp
     BITHockeyManager *hockeyManager = [BITHockeyManager sharedHockeyManager];
     [hockeyManager configureWithBetaIdentifier:@"a7501c5e67fff3beee23f67033660276" liveIdentifier:@"56af34ce7a237742eb1db93c348cfb95" delegate:self];
+    hockeyManager.crashManager.delegate = self;
+    hockeyManager.updateManager.delegate = self;
     [hockeyManager startManager];
     
-    // Make the window visible
+    // Use verbose logging if last session crashed
+    if (hockeyManager.crashManager.didCrashInLastSession) {
+        ddLogLevel = LOG_LEVEL_VERBOSE;
+    }
+    
     [_window makeKeyAndVisible];
 
     return YES;
@@ -107,10 +167,7 @@ int ddLogLevel = LOG_LEVEL_WARN;
     return YES;
 }
 
-- (UIViewController *)application:(UIApplication *)application viewControllerWithRestorationIdentifierPath:(NSArray *)identifierComponents coder:(NSCoder *)coder {
-    // Create the base view controller heirarchy if it does not exist
-    [self setupViewControllers];
-    
+- (UIViewController *)application:(UIApplication *)application viewControllerWithRestorationIdentifierPath:(NSArray *)identifierComponents coder:(NSCoder *)coder {    
     // Restore the state of the view controllers
     NSString *identifier = [identifierComponents lastObject];
     if ([identifier isEqualToString:MCSplitViewIdentifier]) {
@@ -122,72 +179,6 @@ int ddLogLevel = LOG_LEVEL_WARN;
     }
     
     return nil;
-}
-
-- (void)setupViewControllers {
-    if (_window != nil)
-        return;
-    
-    DDLogInfo(@"(%@): Instantiating view heirarchy", [[UIApplication sharedApplication] delegate]);
-    
-    _window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
-    
-    // Create the master list view controller if it doesn't exist
-    _listViewController = [[MCServerListViewController alloc] init];
-    _listViewController.restorationIdentifier = MCServerListIdentifier;
-    
-    _navigationController = [[UINavigationController alloc] initWithRootViewController:_listViewController];
-    _navigationController.restorationIdentifier = MCMasterNavigationIdentifier;
-    
-    if ([[UIDevice currentDevice] userInterfaceIdiom] == UIUserInterfaceIdiomPhone) {
-        // Make the master view controller the root view controller
-        _listViewController.detailNavigationController = _navigationController;
-        _window.rootViewController = _navigationController;
-    } else {
-        // Create the detail view controller
-        UINavigationController *detailNavigationController = [[UINavigationController alloc] init];
-        _listViewController.detailNavigationController = detailNavigationController;
-    	
-        // Create a split view controller with master and detail view controllers
-        _splitViewController = [[UISplitViewController alloc] init];
-        _splitViewController.restorationIdentifier = MCSplitViewIdentifier;
-        _splitViewController.delegate = _listViewController;
-        _splitViewController.viewControllers = @[_navigationController, detailNavigationController];
-        
-        // Make the split view controller the root view controller
-        _window.rootViewController = _splitViewController;
-    }
-    
-    // Global appearance tweaks
-    [[UINavigationBar appearance] setTintColor:[UIColor darkGrayColor]];
-}
-
-#pragma mark - CocoaLumberjack
-
-- (void)setupLogging {
-    if (_fileLogger != nil)
-        return;
-    
-    [DDLog addLogger:[DDASLLogger sharedInstance]];
-#if defined(CONFIGURATION_Debug) || defined(CONFIGURATION_AdHoc)
-    [DDLog addLogger:[DDTTYLogger sharedInstance]];
-#endif
-    
-    _fileLogger = [[DDFileLogger alloc] init];
-    _fileLogger.maximumFileSize = (1024 * 1024); // 1 MB
-    _fileLogger.rollingFrequency = 0;
-    _fileLogger.logFileManager.maximumNumberOfLogFiles = 5;
-    
-    DDLogInfo(@"(%@): Instantiating file logger: %@", [[UIApplication sharedApplication] delegate], _fileLogger);
-    
-    // Roll log (upon application launch
-    [_fileLogger performSelector:@selector(currentLogFileHandle)];
-    [_fileLogger rollLogFile];
-    [_fileLogger performSelector:@selector(currentLogFileHandle)];
-    
-    DDLogInfo(@"(%@): Rolled log file for logger: %@", [[UIApplication sharedApplication] delegate], _fileLogger);
-    
-    [DDLog addLogger:_fileLogger];
 }
 
 #pragma mark - HockeyApp
@@ -211,7 +202,7 @@ int ddLogLevel = LOG_LEVEL_WARN;
             NSError *error;
             NSString *applicationLog = [NSString stringWithContentsOfFile:logs[logs.count - 2] encoding:NSUTF8StringEncoding error:&error];
             if (error) {
-                DDLogError(@"Error loading log file: %@", error);
+                DDLogError(@"(%@): Error loading log file: %@", [[UIApplication sharedApplication] delegate], error);
             }
             
             return applicationLog;
